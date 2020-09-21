1e77a1f34f5aa868da9c8c0a92adbdb837afd7b4
Justin-sky <284139554@qq.com>
2020-09-21 20:30:59 +0800

    添加cache, 减少GC

--- TsProj/src/framework/ui/UIFactory.ts
+++ TsProj/src/framework/ui/UIFactory.ts
@@ -17,39 +17,44 @@ const CS = require('csharp');
 
 export class UIFactory{
 
+    public static uiCache:Map<string,UIPanel> = new Map<string,UIPanel>();
+
     public static createUI(pkg:string, name:string){
         console.log(`create UI: ${pkg}:${name}`)
         let comp = CS.FairyGUI.UIPackage.CreateObject(pkg, name).asCom
         
-        let ui:UIPanel = null;
+        let ui:UIPanel = this.uiCache.get(name);
 
-        switch (name){
+        if(!ui){
+            switch (name){
+                //common
+                case commonUI.UIUINoticeWin:
+                    ui = new UIMsgBox();
+                    break;
+                case commonUI.UILoadingPage:
+                    ui = new UILoading();
+                    break;
+    
+                    //login
+                case loginUI.UILoginPage:
+                    ui = new UILoginPage();
+                    break;
+                case loginUI.UISelServerWin:
+                    ui = new UISelServerWin();
+                    break;
+                    
+                    //home
+                case homeUI.UIHomePage:
+                    ui = new UIHomePage();
+                    break;
+                case homeUI.UIShopPage:
+                    ui = new UIShopPage();
+                    break;
+            }
 
-            //common
-            case commonUI.UIUINoticeWin:
-                ui = new UIMsgBox();
-                break;
-            case commonUI.UILoadingPage:
-                ui = new UILoading();
-                break;
-
-                //login
-            case loginUI.UILoginPage:
-                ui = new UILoginPage();
-                break;
-            case loginUI.UISelServerWin:
-                ui = new UISelServerWin();
-                break;
-                
-                //home
-            case homeUI.UIHomePage:
-                ui = new UIHomePage();
-                break;
-            case homeUI.UIShopPage:
-                ui = new UIShopPage();
-                break;
+            this.uiCache.set(name, ui);
         }
-
+        
         if(ui!=null){
             ui.fui = comp;
             ui.name = name;
